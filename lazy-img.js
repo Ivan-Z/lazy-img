36555f09ce0a1cc8b42e45d7be3a23478b45d79d
Ivan Zdane <izdane@gmail.com>
2019-02-15 15:31:21 -0500

    Setters and Getters for image tag atributes

--- lazy-img.js
+++ lazy-img.js
@@ -4,62 +4,99 @@ template.innerHTML = `<img id="image"/>`;
 
 
 class LazyImage extends HTMLElement {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    // Element Properties
-    this.src = '';
-    this.alt = '';
+		// Element Properties
+		this.src = '';
+		this.alt = '';
 
-    //Set up observer for lazy loading
-    this.observerCallback = this.observerCallback.bind(this);
-    this.visible = false;
+		//Set up observer for lazy loading
+		this.observerCallback = this.observerCallback.bind(this);
+		this.visible = false;
 
-  }
+	}
 
-  //Called everytime the element is connected to the DOM
-  connectedCallback() {
-    // Set the element properties from the DOM 
-    this.src = this.getAttribute('src') || this.src;
-    this.alt = this.getAttribute('alt') || this.alt;
+	//Setters and Getters for image tag attributes
 
-    // Create Shadow Root if it does not exist
-    if (!this.shadowRoot) {
-      this.attachShadow({mode: 'open'});
-      this.shadowRoot.appendChild(template.content.cloneNode(true));
-      this.shadowImage = this.shadowRoot.getElementById('image')
-    }
+	//Prevents attribute double setting
+	safeSetAttribute(attribute, value) {
+		if (this.getAttribute(attribute) !== value) this.setAttribute(attribute, value);
+	}
 
-    // Set the attributes of the shadow image
-    this.shadowImage.src = this.src;
-    this.shadowImage.alt = this.alt;
+	set src(value) {
+		this.safeSetAttribute('src', value);
+		if (this.shadowImage) this.shadowImage.src = value;
+	}
 
-    if ('IntersectionObserver' in window){
-    	this.initIntersectionObserver();
-    } else {
-    	this.visible = true;
-    } 
+	get src() {
+		return this.getAttribute('src')
+	}
 
-  }
+	set alt(value) {
+		this.safeSetAttribute('alt', value);
+		// Set image alt
+		if (this.shadowImage) this.shadowImage.alt = value;
+	}
+
+	get alt() {
+		return this.getAttribute('alt')
+	}
+
+
+	//Called everytime the element is connected to the DOM
+	connectedCallback() {
+		// Set the element properties from the DOM 
+		this.src = this.getAttribute('src') || this.src;
+		this.alt = this.getAttribute('alt') || this.alt;
+
+		// Create Shadow Root if it does not exist
+		if (!this.shadowRoot) {
+			this.attachShadow({
+				mode: 'open'
+			});
+			this.shadowRoot.appendChild(template.content.cloneNode(true));
+			this.shadowImage = this.shadowRoot.getElementById('image')
+		}
+
+		// Set the attributes of the shadow image
+		this.shadowImage.src = this.src;
+		this.shadowImage.alt = this.alt;
+
+		if ('IntersectionObserver' in window) {
+			this.initIntersectionObserver();
+		} else {
+			this.visible = true;
+		}
+
+	}
+
+
+	static get observedAttributes() {
+		return ['src', 'alt'];
+	}
+
+	attributeChangedCallback(name, oldVal, newVal) {
+		this[name] = newVal;
+	}
 
 	observerCallback(entries) {
-	  if (entries.some(visisble)) this.visible = true
+		//if (entries.some(this.visisble)) this.visible = true
 	}
 
 	initIntersectionObserver() {
-	  if (this.observer) return;	
-	  this.observer =
-	    new IntersectionObserver(this.observerCallback);
-	  this.observer.observe(this);
+		if (this.observer) return;
+		this.observer =
+			new IntersectionObserver(this.observerCallback);
+		this.observer.observe(this);
 	}
 
 
 	//Called each time the element is disconnected from the DOM
-	disconnectedCallback() {
-  	}
+	disconnectedCallback() {}
 
 }
- 
+
 
 var component = () => customElements.define(tagName, LazyImage);
 window.WebComponents ? window.WebComponents.waitFor(component) : component();
